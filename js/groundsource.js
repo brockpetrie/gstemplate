42a5b7712c992e7d65595c47aae45c8fda36d444
Brock Petrie <brockpetrie@gmail.com>
2013-08-12 20:57:42 -0500

    Headway on the Campaign Builder

--- js/groundsource.js
+++ js/groundsource.js
@@ -1,8 +1,53 @@
-$(document).ready(function() {
+(function($){
 
-	/*$.extend($.fn, {
-		fancySelect: function
-	});*/
+	$.fn.fancySelect = function() {
+		var targ = $(this),
+			options = $('option,optgroup', targ);
+
+		targ.css({'visibility': 'hidden', 'display': 'none'});
+
+		var drop = $('<div class="dropdown" />');
+		var inner = $('<div class="dropdownInner" />').appendTo(drop);
+		var label = $('<span class="selectedLabel" />').appendTo(inner);
+		var optionlist = $('<ul class="options" />');
+
+		options.each(function(n) {
+			var item = $(this),
+				caption = $(this).attr('data-caption');
+				li = $('<li />');
+
+			if (item.is('optgroup')) {
+				li.text(item.attr('label')).addClass('group');
+			} else {
+				li.text(item.text());
+				if (caption) $('<i />').text(caption).appendTo(li);
+				li.on('click', function() {
+					var select = $(this).parent();
+					item.prop('selected', true);
+					select.find('li.selected').removeClass('selected');
+					$(this).addClass('selected');
+					label.html($(this).html());
+					dropdown.hide();
+				});
+			}
+
+			if (item.attr('selected')) {
+				li.addClass('selected');
+				label.html(li.html());
+			}
+			if (item.parent().is('optgroup')) li.addClass('groupie');
+
+			li.appendTo(optionlist);
+		});
+
+		optionlist.appendTo(drop);
+		drop.insertAfter(targ);
+	};
+
+})(jQuery); 
+
+
+$(document).ready(function() {
 
 	$('input[type="checkbox"]').each(function() {
 		var classy = $(this).hasClass('toggle') ? 'toggle' : 'checkbox';
@@ -25,51 +70,6 @@ $(document).ready(function() {
 		lbl.prepend('<span />').appendTo(parent);
 	});
 
-	$('select:not([multiple])').each(function(i) {
-		var targ = $(this),
-			options = $('option,optgroup', targ);
-
-		targ.css({'visibility': 'hidden', 'display': 'none'}).attr('data-ref', i);
-
-		var drop = $('<div class="dropdown" />');
-		var inner = $('<div class="dropdownInner" />').appendTo(drop);
-		var label = $('<span class="selectedLabel" />').appendTo(inner);
-		var optionlist = $('<ul class="options" />');
-
-		options.each(function(n) {
-			var item = $(this),
-				key = i+''+n;
-				li = $('<li />');
-
-			if (item.is('optgroup')) {
-				li.text(item.attr('label')).addClass('group');
-			} else {
-				li.text(item.text());
-				item.attr('data-ref', key);
-				li.attr('data-ref', key);
-				li.on('click', function() {
-					var select = $(this).parent();
-					item.prop('selected', true);
-					select.find('li.selected').removeClass('selected');
-					$(this).addClass('selected');
-					label.text($(this).text());
-					dropdown.hide();
-				});
-			}
-			//if (item.attr('value')) li.attr('data-value', item.attr('value'));
-			if (item.attr('selected')) {
-				li.addClass('selected');
-				label.text(item.text());
-			}
-			if (item.parent().is('optgroup')) li.addClass('groupie');
-
-			li.appendTo(optionlist);
-		});
-
-		optionlist.appendTo(drop);
-		drop.insertAfter(targ);
-	});
-
 
 	var dropdown = {
 
@@ -86,7 +86,7 @@ $(document).ready(function() {
 			dropdown.hide();
 
 			if (isOpen) return;
-			targ.addClass('opened');
+			targ.addClass('opened').focus();
 			//if (targ.not('.disabled').not('.opened')) targ.addClass('opened');
 			var targetGroup = event ? $(event.target).parents().addBack() : null;
 			if (isOpen || targetGroup.is('ul.options')) targ.removeClass('opened');
@@ -102,10 +102,77 @@ $(document).ready(function() {
 				}
 			}
 
-			$(document).find('.dropdown.opened').removeClass('opened');
+			$(document).find('.dropdown.opened').removeClass('opened').blur();
 		}
 	};
 
+
+	var questionBuilder = {
+
+		stage: 		$('#qBuilder'),
+		template: 	$('#qBuilder li.question.template'),
+		addBtn: 	$('#qBuilderBtnAdd'),
+
+		init: function() {
+			this.template.hide();
+			this.stage.sortable();
+
+			questionBuilder.add();
+
+			this.addBtn.click(function(event) {
+				event.preventDefault();
+				questionBuilder.add();
+			});
+		},
+
+		add: function() {
+			var newQ = this.template.clone().removeClass('template').appendTo(this.stage).show();
+			$('textarea.questionText', newQ).autosize();
+			$('select', newQ).fancySelect();
+			$('.questionTags', newQ).tagit({
+				placeholderText: 'Optional'
+			});
+			//$('.questionTags', newQ).tagsInput();
+			this.stage.sortable('refresh');
+			var del = $('.btnRemove', newQ);
+			del.click(function(event) {
+				if ($(this).parent().hasClass('focused')) {
+					if (confirm('Are you sure you want to remove this question?')) {
+						$(this).parent().remove();
+						questionBuilder.stage.sortable('refresh');
+					} else {
+						event.preventDefault();
+					}
+				} else {
+					event.preventDefault();
+				}
+			});
+		},
+
+		kill: function(el) {
+			// Nothing?
+		},
+
+		focus: function() {
+			var question = event ? $(event.target).closest('.question') : null;
+
+			if (question.not('.focused')) {
+				$('.question.focused').not(question).removeClass('focused').children('.options').slideUp(250);
+				question.addClass('focused');
+				question.children('.options').slideDown(300);
+			}
+		}
+
+	};
+
+
 	$(document).on('click.dropdown', '.dropdown', dropdown.show);
 	$(document).on('click.dropdown', dropdown.hide);
+
+	$(document).on('click.question', questionBuilder.focus);
+
+	questionBuilder.init();
+
+	$('select:not([multiple]):not(.notFancy)').fancySelect();
+
 });
